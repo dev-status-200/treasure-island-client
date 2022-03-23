6728b44ffd6dacf96c1ae685782f6b87d5f83bf7
dev-status-200 <dev.status.200@example.com>
2022-03-23 19:55:48 +0500

    asd

--- components/layouts/LoginLayout.js
+++ components/layouts/LoginLayout.js
@@ -7,9 +7,20 @@ const LoginLayout = () => {
     <div className='signin_styles '>
         <Container className='bg' fluid>
             <Row className='justify-content-md-center pt-5'>
-                <Col md="auto pt-5">
-                    <Image src={'/assets/images/white png logo.png'} width={160} height={70} />
+            <div className='space'></div>
+                <Col md="auto">
+                    <Image src={'/assets/images/white png logo.png'} width={140} height={70} />
                 </Col>
+            </Row>
+            <Row className='justify-content-md-center pt-4'>
+            <Col md="auto">
+              <input placeholder='Username' className='input-field' />
+            </Col>
+            </Row>
+            <Row className='justify-content-md-center pt-4'>
+            <Col md="auto">
+              <input placeholder='Password' type='password' className='input-field' />
+            </Col>
             </Row>
         </Container>
     </div>
