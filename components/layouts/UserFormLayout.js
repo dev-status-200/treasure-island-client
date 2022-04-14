e3091e3f56e48906f9ed48d372306505dbf742d4
dev-status-200 <dev.status.200@example.com>
2022-04-15 04:48:05 +0500

    updated

--- components/layouts/UserFormLayout.js
+++ components/layouts/UserFormLayout.js
@@ -1,23 +1,30 @@
-import React, { useState, useEffect } from 'react'
-import { Container, Row, Col, Table, Modal, Button, Form, Spinner, FormControl } from 'react-bootstrap'
-import axios from 'axios'
-import Cookies from 'js-cookie'
+import React, { useState, useEffect } from 'react';
+import { Container, Row, Col, Table, Modal, Button, Form, Spinner, FormControl } from 'react-bootstrap';
+import axios from 'axios';
+
+import Cookies from 'js-cookie';
+
 import NumberFormat from "react-number-format";
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/router';
 
 const UserFormLayout = () => {
-    
+
     const router = useRouter();
 
     const [available, setAvailable] = useState(true);
     const [success, setSuccess] = useState(false);
 
     const [mailList, setMailList] = useState([]);
-    
+
     const [load, setLoad] = useState(false);
 
     const [mailWarn, setMailWarn] = useState(false);
     const [phoneWarn, setPhoneWarn] = useState(false);
+
+    const [customerType, setCustomerType] = useState("")
+    const [checkNew, setCheckNew] = useState("");
+    const [serviceShow, setServiceShow] = useState("");
+    const [detailCheck, setDetailCheck] = useState("");
 
     useEffect(() => {
         console.log(router.asPath.slice(13))
@@ -32,21 +39,26 @@ const UserFormLayout = () => {
         })
         getMail();
     }, [])
-    
+
     const getMail = async() => {
         let res = await axios.get(process.env.NEXT_PUBLIC_TI_CUSTOMERS_EMAILS).then((x)=>(x.data));
         console.log(res)
         setMailList(res);
     }
-    const [f_name  , setF_name    ] = useState('');
-    const [l_name  , setL_Name    ] = useState('');
-    const [email   , setEmail     ] = useState('');
-    const [phone   , setPhone     ] = useState('');
-    const [address , setAddress   ] = useState('');
-    const [location , setLocation   ] = useState('location-1');
+    const [f_name      , setF_name    ] = useState('');
+    const [l_name      , setL_Name    ] = useState('');
+    const [email       , setEmail     ] = useState('');
+    const [phone       , setPhone     ] = useState('');
+    const [address     , setAddress   ] = useState('');
+    const [location    , setLocation  ] = useState('location-1');
+    const [make        , setMake      ] = useState('');
+    const [model       , setModel     ] = useState('');
+    const [year        , setYear      ] = useState('');
+    const [regio       , setRegio     ] = useState('');
+    const [service     , setService   ] = useState('Service 1');
+    const [description , setDescrition] = useState('');
 
-
-    const addAgent = async(e) => {
+    const addAgent = (e) => {
         e.preventDefault();
         let mailWarning = false;
         let phoneWarning = false;
@@ -63,102 +75,225 @@ const UserFormLayout = () => {
             phoneWarning==true?setPhoneWarn(true):setPhoneWarn(false);
 
         }else{
+            setServiceShow("show");
+            //setLoad(true);
+
+            // await axios.post(process.env.NEXT_PUBLIC_TI_ADD_CUSTOMERS, {
+            //     f_name:f_name, l_name:l_name, photo:"https://res.cloudinary.com/abdullah7c/image/upload/v1643040095/images_djois2.png",
+            //     email:email, shop_id:location, phone:phone, address:address, id:router.asPath.slice(13)
+            // }).then((x)=>{
+            //     setLoad(false);
+            //     setSuccess(true);
+            // })
+        }
+    }
+    const submitForm = async(e) => {
+        e.preventDefault();
             setLoad(true);
+
             await axios.post(process.env.NEXT_PUBLIC_TI_ADD_CUSTOMERS, {
                 f_name:f_name, l_name:l_name, photo:"https://res.cloudinary.com/abdullah7c/image/upload/v1643040095/images_djois2.png",
-                email:email, shop_id:location, phone:phone, address:address, id:router.asPath.slice(13)
+                email:email, shop_id:location, phone:phone, address:address, id:router.asPath.slice(13), make:make, model:model, year:year, regio:regio,
+                service:service, description:description
             }).then((x)=>{
                 setLoad(false);
                 setSuccess(true);
             })
+    }
+    const checkCustomerType = () => {
+        if(checkNew=="new"){
+            setCustomerType("new");
+        }else if(checkNew=="old"){
+            setCustomerType("old");
         }
     }
-
+    const checkDetails = (e) => {
+        e.preventDefault();
+        setDetailCheck("ok")
+    }
   return (
     <div className='online-form pt-5' >
     {(available==true  && success==false) && 
     <div>
     <div className='text-center'>
-    <img src={'/assets/images/white png logo.png'} width={140} height={70} />
-    <h3 className='wh'>Online Registration</h3>
-</div>    
-<Container className='box mt-4 px-4 py-5' style={{maxWidth:'600px'}}>
-<Form onSubmit={addAgent}>
-<Row>
-    <Col>
-        <Form.Group className="mb-3" controlId="First Name">
-            <Form.Label>First Name</Form.Label>
-            <input 
-                type="text" placeholder="first Name..." required value={f_name} onChange={(e)=>setF_name(e.target.value)} 
-                style={{border:'1px solid silver', borderRadius:'5px', width:"100%", height:'39px', paddingLeft:'15px'}}
-            />
-        </Form.Group>
-    </Col>
-    <Col>
-        <Form.Group className="mb-3" controlId="Last Name">
-            <Form.Label>Last Name</Form.Label>
-            <input 
-                style={{border:'1px solid silver', borderRadius:'5px', width:"100%", height:'39px', paddingLeft:'15px'}}
-                type="text" placeholder="last Name..." required value={l_name} onChange={(e)=>setL_Name(e.target.value)}
-            />
-        </Form.Group>                
-    </Col>
-</Row>      
-<Row>
-    <Col>
-        <Form.Group className="mb-3" controlId="Email">
-            <Form.Label>Email</Form.Label>
-            <input
-            style={{border:'1px solid silver', borderRadius:'5px', width:"100%", height:'39px', paddingLeft:'15px'}}
-            type="email" required placeholder="email..." value={email} onChange={(e)=>setEmail(e.target.value)} />
-            {mailWarn==true && <Form.Text style={{color:'crimson'}}> Email Not Unique  </Form.Text>}
-            </Form.Group>
-            </Col>
-            <Col>
-            <Form.Group className="mb-3" controlId="Shop ID">
-                <Form.Label>Shop ID</Form.Label>
-                <select aria-label="Default select example" required value={location} onChange={(e)=>setLocation(e.target.value)}
-                    style={{border:'1px solid silver', borderRadius:'5px', width:"100%", height:'39px', paddingLeft:'5px', backgroundColor:'white'}}
-                    >
-                        <option value="location-1">Location-1</option>
-                        <option value="location-2">Location-2</option>
-                    </select>
-            </Form.Group>                
-        </Col>
-</Row>
-<Row>
+            <img className='mb-3' src={'/assets/images/white png logo.png'} width={140} height={70} />
+            {(available==true  && success==false && customerType=="") && <h3 className='wh'>Welcome To Our Shop</h3>}
+            {(customerType=="new" && serviceShow=="") && <h3 className='wh'>Your Information for Registration</h3>}
+            {(customerType=="new" && serviceShow=="show" && detailCheck=="") && <h3 className='wh'>Car Details</h3>}
+            {(customerType=="new" && serviceShow=="show" && detailCheck=="ok") && <h3 className='wh'>How Can We Help You?</h3>}
+        </div>
+        {   customerType=="" &&
+            <Container className='box mt-4 px-5 py-5' style={{maxWidth:'600px'}}>
+            <Row className='px-5 py-3'>
+                <Col>
+                    <Form.Group className="mb-3" controlId="old">
+                        <Form.Check type="checkbox" label=" Existing Customer" checked={checkNew=="old"?true:false} onChange={()=>setCheckNew("old")} />
+                    </Form.Group>
+                </Col>
+                <Col>
+                    <Form.Group  className="mb-3" controlId="new">
+                        <Form.Check type="checkbox" label=" New Customer" checked={checkNew=="new"?true:false} onChange={()=>setCheckNew("new")} />
+                    </Form.Group>
+                </Col>
+            </Row>
+            <Row>
+                <Col style={{textAlign:'center'}}>
+                    <Button className='px-5' onClick={checkCustomerType}>NEXT</Button>
+                </Col>
+            </Row>
+            </Container>
+        }
+        {   (customerType=="new" && serviceShow=="") &&
+            <Container className='box mt-4 px-4 py-5' style={{maxWidth:'600px'}}>
+                <Form onSubmit={addAgent}>
+                <Row>
+                    <Col>
+                        <Form.Group className="mb-3" controlId="First Name">
+                            <Form.Label>First Name</Form.Label>
+                            <input 
+                                type="text" placeholder="first Name..." required value={f_name} onChange={(e)=>setF_name(e.target.value)} 
+                                style={{border:'1px solid silver', borderRadius:'5px', width:"100%", height:'39px', paddingLeft:'15px'}}
+                            />
+                        </Form.Group>
+                    </Col>
+                    <Col>
+                        <Form.Group className="mb-3" controlId="Last Name">
+                            <Form.Label>Last Name</Form.Label>
+                            <input 
+                                style={{border:'1px solid silver', borderRadius:'5px', width:"100%", height:'39px', paddingLeft:'15px'}}
+                                type="text" placeholder="last Name..." required value={l_name} onChange={(e)=>setL_Name(e.target.value)}
+                            />
+                        </Form.Group>                
+                    </Col>
+                </Row>      
+                <Row>
+                    <Col>
+                        <Form.Group className="mb-3" controlId="Email">
+                            <Form.Label>Email</Form.Label>
+                            <input
+                            style={{border:'1px solid silver', borderRadius:'5px', width:"100%", height:'39px', paddingLeft:'15px'}}
+                            type="email" required placeholder="email..." value={email} onChange={(e)=>setEmail(e.target.value)} />
+                            {mailWarn==true && <Form.Text style={{color:'crimson'}}> Email Not Unique  </Form.Text>}
+                        </Form.Group>
+                    </Col>
+                    <Col>
+                        <Form.Group className="mb-3" controlId="Shop ID">
+                            <Form.Label>Shop ID</Form.Label>
+                            <select aria-label="Default select example" required value={location} onChange={(e)=>setLocation(e.target.value)}
+                                style={{border:'1px solid silver', borderRadius:'5px', width:"100%", height:'39px', paddingLeft:'5px', backgroundColor:'white'}}
+                            >
+                                    <option value="location-1">Location-1</option>
+                                    <option value="location-2">Location-2</option>
+                                </select>
+                        </Form.Group>                
+                    </Col>
+                </Row>
+                <Row>
 
-</Row>
-<Row>
-    <Col md={6}>
-        <Form.Group className="mb-3" controlId="Phone">
-            <Form.Label>Phone</Form.Label>
-            <NumberFormat
-                    format="(+#)###-###-####"
-                    style={{border:'1px solid silver', borderRadius:'5px', width:"100%", height:'39px', paddingLeft:'15px'}}
-                    mask="_"
-                    allowEmptyFormatting={true}
-                    required value={phone} onChange={(e)=>setPhone(e.target.value)}
-                />
-                {phoneWarn==true && <Form.Text style={{color:'crimson'}}> Phone Already Exists </Form.Text>}
-        </Form.Group>
-    </Col>
-</Row>
-<Row>
-    <Col>
-        <Form.Group className="mb-3" controlId="Address">
-            <Form.Label>Detailed Address</Form.Label>
-            <input
-            style={{border:'1px solid silver', borderRadius:'5px', width:"100%", height:'39px', paddingLeft:'15px'}}
-            type="text" placeholder="address..." required value={address} onChange={(e)=>setAddress(e.target.value)}/>
-        </Form.Group>
-    </Col>
-</Row>
-    <Button variant="primary" type="submit">
-        {load==true?<Spinner className='mx-4' as="span" animation="border" size="sm" role="status" aria-hidden="true"/>:'Create'}
-    </Button>
-</Form>
-</Container>
+                </Row>
+                <Row>
+                    <Col md={6}>
+                        <Form.Group className="mb-3" controlId="Phone">
+                            <Form.Label>Phone</Form.Label>
+                            <NumberFormat
+                                    format="(+#)###-###-####"
+                                    style={{border:'1px solid silver', borderRadius:'5px', width:"100%", height:'39px', paddingLeft:'15px'}}
+                                    mask="_"
+                                    allowEmptyFormatting={true}
+                                    required value={phone} onChange={(e)=>setPhone(e.target.value)}
+                                />
+                                {phoneWarn==true && <Form.Text style={{color:'crimson'}}> Phone Already Exists </Form.Text>}
+                        </Form.Group>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Form.Group className="mb-3" controlId="Address">
+                            <Form.Label>Detailed Address</Form.Label>
+                            <input
+                            style={{border:'1px solid silver', borderRadius:'5px', width:"100%", height:'39px', paddingLeft:'15px'}}
+                            type="text" placeholder="address..." required value={address} onChange={(e)=>setAddress(e.target.value)}/>
+                        </Form.Group>
+                    </Col>
+                </Row>
+                    <Button variant="primary" type="submit">
+                        {load==true?<Spinner className='mx-4' as="span" animation="border" size="sm" role="status" aria-hidden="true"/>:'Next'}
+                    </Button>
+                </Form>
+            </Container>
+        }
+        {
+            (customerType=="new" && serviceShow=="show" && detailCheck=="") &&
+            <Container className='box mt-4 px-5 py-5' style={{maxWidth:'800px'}}>
+            <Form onSubmit={checkDetails}>
+            <Row>
+                <Col>
+                <Form.Group className="mb-3" controlId="make">
+                    <Form.Label>Make</Form.Label>
+                    <Form.Control type="text" placeholder="" value={make} onChange={(e)=>setMake(e.target.value)} required />
+                </Form.Group>
+                </Col>
+                <Col>
+                <Form.Group className="mb-3" controlId="make">
+                    <Form.Label>Model</Form.Label>
+                    <Form.Control type="text" placeholder="" value={model} onChange={(e)=>setModel(e.target.value)} required />
+                </Form.Group>
+                </Col>
+                <Col>
+                <Form.Group className="mb-3" controlId="make">
+                    <Form.Label>Year</Form.Label>
+                    <Form.Control type="text" placeholder="" value={year} onChange={(e)=>setYear(e.target.value)} required />
+                </Form.Group>
+                </Col>
+                <Col>
+                <Form.Group className="mb-3" controlId="make">
+                    <Form.Label>Regio/Vin</Form.Label>
+                    <Form.Control type="text" placeholder="" value={regio} onChange={(e)=>setRegio(e.target.value)} required />
+                </Form.Group>
+                </Col>
+
+            </Row>
+            <Row className='mt-3'>
+                <Col style={{textAlign:'center'}}>
+                    <Button className='px-5' type="submit">NEXT</Button>
+                </Col>
+            </Row>
+            </Form>
+            </Container>
+        }
+        {
+            (customerType=="new" && serviceShow=="show" && detailCheck=="ok") &&
+            <Container className='box mt-4 px-5 py-5' style={{maxWidth:'600px'}}>
+                <Form onSubmit={submitForm}>
+                <Row className="justify-content-md-center">
+                    <Col md={8}>
+                    <Form.Label>Service</Form.Label>
+                        <Form.Select onChange={(e)=>setService(e.target.value)} required>
+                            <option value="Service 1">Service 1</option>
+                            <option value="Service 2">Service 2</option>
+                            <option value="Service 3">Service 3</option>
+                        </Form.Select>
+                    </Col>
+                </Row>
+                <Row className="justify-content-md-center mt-3">
+                    <Col md={8}>
+                    <Form.Group className="mb-1 " controlId="exampleForm.ControlTextarea1">
+                        <Form.Label>Description</Form.Label>
+                        <Form.Control as="textarea" rows={3} required value={description} onChange={(e)=>setDescrition(e.target.value)} />
+                    </Form.Group>
+                    </Col>
+                </Row>
+                <Row className="justify-content-md-center mt-3">
+                    <Col md="auto">
+                        <Button className='px-4' type="submit">{load?<Spinner animation="border" size="sm" />:"Submit"}</Button>
+                    </Col>
+                </Row>
+                </Form>
+            </Container>
+        }
+        {   customerType=="old" &&
+            <div>OLD CUSTOMER</div>
+        }
     </div>
     }
     {
@@ -172,4 +307,3 @@ const UserFormLayout = () => {
 }
 
 export default UserFormLayout
-
